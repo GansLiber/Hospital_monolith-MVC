dadbf7c1bae0a111ff4a5ef5ef5cefa067e77463
zazaz <zazaza993@mail.ru>
2023-04-27 23:21:43 +0700

    заверстал бутстрапом авторизацию

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -11,11 +11,11 @@ use Src\Auth\Auth;
 
 class Site
 {
-    public function index(Request $request): string
-    {
-        $posts = Post::where('id', $request->id)->get();
-        return (new View())->render('site.post', ['posts' => $posts]);
-    }
+//    public function index(Request $request): string
+//    {
+//        $posts = Post::where('id', $request->id)->get();
+//        return (new View())->render('site.post', ['posts' => $posts]);
+//    }
 
     public function hello(): string
     {
@@ -33,6 +33,7 @@ class Site
 
     public function signup(Request $request): string
     {
+        echo ($request->method), '&nbsp', 'gg';
         if ($request->method === 'POST' && User::create($request->all())) {
             if (Auth::attempt($request->all())) {
                 app()->route->redirect('/hello');
--- views/site/login.php
+++ views/site/login.php
@@ -1,13 +1,28 @@
-<h2>Авторизация</h2>
-<h3><?= $message ?? ''; ?></h3>
+<div class="container">
+  <div class="row justify-content-center mt-5">
+    <div class="col-md-6 col-lg-4">
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title text-center mb-4">Авторизация</h2>
+            <?php if(isset($message)): ?>
+              <h3 class="text-center text-danger"><?= $message ?></h3>
+            <?php endif; ?>
+            <?php if(!app()->auth::check()): ?>
+              <form method="post">
+                <div class="form-group">
+                  <label for="login">Логин:</label>
+                  <input type="text" class="form-control" id="login" name="login">
+                </div>
+                <div class="form-group">
+                  <label for="password">Пароль:</label>
+                  <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Войти</button>
+              </form>
+            <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
-    ?>
-    <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
-    </form>
-<?php endif;
