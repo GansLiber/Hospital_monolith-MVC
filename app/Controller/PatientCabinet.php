28c45c2287edf538c17a7be2a26ab8958ccfe082
gr411_vanpa <zazaza993@mail.ru>
2023-05-04 14:56:08 +0700

    немного верстки на страницах пациентов

--- app/Controller/PatientCabinet.php
+++ app/Controller/PatientCabinet.php
@@ -2,14 +2,18 @@
 
 namespace Controller;
 
+use Model\Appointment;
 use Src\Request;
 use Model\Patient;
 use Src\View;
 
 class PatientCabinet
 {
-    public function patientCabinet(Request $request){
+    public function patientCabinet(Request $request): string
+    {
+        $patientAppointments = Appointment::where('id_patient',$request->id)->get();
+        var_dump($patientAppointments); die();
         $patient = Patient::where('id_patient', $request->id)->first();
-        return (new View())->render('site.patientCabinet', ['patient'=>$patient]);
+        return new View('site.patientCabinet', ['patient'=>$patient]);
     }
 }
--- views/site/patientCabinet.php
+++ views/site/patientCabinet.php
@@ -1,2 +1,27 @@
-<?php
-echo ($patient->name);
+<h2>gabella</h2>
+<div class="container" style='margin-top: 30px'>
+    <div class="row">
+        <div class="col-md-4">
+            <h4 class="card-title">Пациент</h4>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <p class="card-text">ФИО: <?= $patient->name ?> <?= $patient->surname ?> <?= $patient->patronymic ?></p>
+                    <p class="card-text">Дата рождения: <?= $patient->date_birth ?> </p>
+                </div>
+            </div>
+        </div>
+            <div class="col-md-8">
+                <h4 class="card-title">Записи</h4>
+                <div class="card">
+                    <div class="card-body">
+                        <?php foreach ($myPatients as $myPatient): ?>
+
+                            <p>ФИО: <?= $myPatient->name ?> <?= $myPatient->surname ?> <?= $myPatient->patronymic ?></p>
+                            <p>Дата записи: <?= $myPatient->pivot->date_time?></p>
+                            <hr>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
